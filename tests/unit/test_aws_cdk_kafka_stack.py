46812974f074e48dca514a87b1b8f3873c717bea
ramonmarrero <ramon.marrero@lanube.io>
2021-11-29 11:05:09 +0100

    Initial commit

---
+++ tests/unit/test_aws_cdk_kafka_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from aws_cdk_kafka.aws_cdk_kafka_stack import AwsCdkKafkaStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in aws_cdk_kafka/aws_cdk_kafka_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = AwsCdkKafkaStack(app, "aws-cdk-kafka")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
